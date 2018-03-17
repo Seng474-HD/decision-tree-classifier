c45773dec34d0d923410315d53eabed2bc70051d
Chris Kelly <cpjkelly@gmail.com>
2018-03-16 20:48:15 -0700

    Finish naive classifier
    No pruning or anything. Just plain wholesome data baby

--- decision_tree_classifier.py
+++ decision_tree_classifier.py
@@ -36,8 +36,7 @@ with open(DATA_FILE_PATH, 'r') as input_file:
         row_idx += 1
 
 end_time = time.time()
-print('Time elapsed: ')
-print(end_time - start_time)
+print('Time elapsed: ', end_time - start_time)
 
 X_train = X[:NUM_TRAIN_ROWS]
 X_test = X[NUM_TRAIN_ROWS:]
@@ -45,7 +44,18 @@ X_test = X[NUM_TRAIN_ROWS:]
 Y_train = Y[:NUM_TRAIN_ROWS]
 Y_test = Y[NUM_TRAIN_ROWS:]
 
-pdb.set_trace()
+print('Fitting classifier on training data')
 clf = tree.DecisionTreeClassifier()
-clf.fit(X, Y)
+clf.fit(X_train, Y_train)
 
+print('Predicting labels of test data')
+preds = clf.predict(X_test)
+baseline_preds = np.zeros(len(preds))
+
+print('Calculating prediction error')
+errors = np.mean(preds != Y_test)
+print('Prediction error: ', errors)
+baseline_errors = np.mean(baseline_preds != Y_test)
+print('Baseline error (always predicting non-fail): ', baseline_errors)
+
+pdb.set_trace()
